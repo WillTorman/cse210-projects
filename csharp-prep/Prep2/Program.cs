93154813ea056331c760fee07b67056c0a8f59db
Will Torman <willtorman@gmail.com>
2024-01-13 21:19:30 -0700

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int percentage = int.Parse(response);
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is an A!");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Your letter grade is a B!");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Your letter grade is a C.");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Your letter grade is a D.");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is a F");
+        }
     }
 }
