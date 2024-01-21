276c8e79df14c4c559a00c3024e2619d64c519d4
Will Torman <willtorman@gmail.com>
2024-01-20 19:43:53 -0700

    prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // do-while loop
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100);
+        int guess;
+        do
+        {
+            // ask for a user guess
+            System.Console.WriteLine($"What is your guess? [BETWEEN 1-100]");
+            guess = int.Parse(Console.ReadLine());
+
+            // check if guess is high
+            if (guess > random)
+            {
+                System.Console.WriteLine($"Too High");
+            }
+
+            // check if guess is low
+            else if (guess < random)
+            {
+                System.Console.WriteLine($"Too Low");
+            }
+
+            // Check if guess is match
+            else
+            {
+                System.Console.WriteLine("Correct");
+            }
+        } while (random != guess);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Create the list
+        List<int> numbers = new List<int>();
+        int answer;
+
+        do
+        {
+            Console.Write("Enter a number (type 0 when finished): ");
+
+            // Parse the user input to an integer
+            if (int.TryParse(Console.ReadLine(), out answer))
+            {
+                if (answer != 0)
+                {
+                    numbers.Add(answer);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+        } while (answer != 0);
+
+        // Check if the list is not empty
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+
+            // Calculate the sum
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+
+            // Find the average
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            // Find the max
+            int max = numbers[0];
+
+            // Iterate to find the maximum number
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine($"The max is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("The list is empty.");
+        }
     }
 }
