e6b61b821820d913e9db500273e5cdee858936de
Will Torman <willtorman@gmail.com>
2024-02-07 21:35:28 -0700

    progress

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,7 +1,55 @@
-public class Fraction 
+using System.Diagnostics;
+
+public class Fraction
 {
-    private int _numerator;
-    private int _denominator;
+    private int _top;
+    private int _bottom;
 
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
 
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public GetTop()
+    {
+
+    }
+
+    public SetTop(top)
+    {
+
+    }
+
+    public GetBottom()
+    {
+
+    }
+
+    public void SetBottom(bottom)
+    {
+        _bottom = ;
+    }
+
+    public string GetFractionString()
+    {
+        Console.WriteLine($"{top}/{bottom}");
+    }
+
+    public double GetDecimalValue()
+    {
+
+    }
 }
