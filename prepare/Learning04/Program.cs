a9971731867533d7a4d5972b51671f960ce0872b
Will Torman <willtorman@gmail.com>
2024-02-21 19:05:29 -0700

    yep

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Student s1 = new Student("id1", "Bob", "CSE");
+        Faculty f1 = new Faculty("id2", "Sue", "BioAg");
+
+        List<Person> people = new List<Person>();
+        people.Add(s1);
+        people.Add(f1);
+
+        foreach (var person in people)
+        {
+            System.Console.WriteLine($"{person.DisplayInfo()}");
+            if (person is Student)
+            {
+                Student student = (Student)person; //type casting (Student)person; This turns the person into a student type.
+                student.Display();
+            }
+            else
+            {
+                Faculty faculty = (Faculty)person;
+                faculty.Display();
+            }
+        }
+    }
+}
+
+
+class Person
+{
+    private string id;
+    private string name;
+
+    public Person(string id, string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public string DisplayInfo()
+    {
+        return $"{name}: {id}";
+    }
+}
+
+
+class Student : Person
+{
+    private string major;
+
+    // base passes the parents constructor variables
+    public Student(string id, string name, string major) : base(id, name)
+    {
+        this.major = major;
+    }
+
+    public void Display()
+    {
+        System.Console.WriteLine($"{DisplayInfo()} - {major}");
+    }
+}
+
+
+class Faculty : Person
+{
+    private string department;
+
+    public Faculty(string id, string name, string department) : base(id, name)
+    {
+        this.department = department;
+    }
+
+    public void Display()
+    {
+        System.Console.WriteLine($"{DisplayInfo()} - {department}");
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        Scripture scripture = new Scripture("");
     }
 
     Scripture.Display();
