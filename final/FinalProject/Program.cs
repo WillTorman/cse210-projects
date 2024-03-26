28e1f478fcffb80ce544e8cb15859e9c4fb57d51
Will Torman <willtorman@gmail.com>
2024-03-25 23:49:21 -0600

    work

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -10,16 +10,17 @@ class Program
 
         while (loop != false)
         {
-            // Menu
+            // Menu & Intro
+            Console.WriteLine("Welcome to the war! We have heard of your bravery and courage. And we will be needing your help to end this war!");
             Console.WriteLine($"\nWhat player type would you like to be?\n");
-            Console.WriteLine($"Player Types:\n    1. Wizard (Magic)\n    2. Warrior (Melee)\n    3. Marksman (Ranged)    6. Quit");
+            Console.WriteLine($"Player Types:\n    1. Wizard (Magic)\n    2. Warrior (Melee)\n    3. Marksman (Ranged)\n    6. Quit\n");
             Console.WriteLine("Select a choice from the menu:");
             int choice = int.Parse(Console.ReadLine());
 
             // Create new goal
             if (choice == 1)
             {
-                
+                Wizard wizard = new Wizard();
             }
 
             else if (choice == 2)
